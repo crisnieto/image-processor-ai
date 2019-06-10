e5ae122af019146dcf091a5ae749a735cae4bdb5
Cristian Nieto <cristian.nieto@mercadolibre.com>
2019-06-09 23:09:01 -0300

    Added ping

---
+++ src/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/crisnieto/image-processor-ai/src/api/controller"
+	"github.com/gin-gonic/gin"
+)
+func main() {
+	router := gin.Default()
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/ping", controller.GetPing)
+	}
+	router.Run()
+}
